OpenMandrivaSoftware/drakx

commit 233eab827963ddaebccf37231944d63809a26a6e

(create_widget) popup advanced settings in dialogs
git-svn-id: svn+ssh://svn.mandriva.com/svn/soft/drakx/trunk@244088 99302b65-d5f7-0310-b3dd-f8cd6f4e3d94

perl-install/interactive/gtk.pm
@@ -409,7 +409,15 @@ sub create_widget {
 	$e->{grow} = 'fill';
 	my $children = [ if_($e->{message}, { type => 'only_label', no_indent => 1, val => \$e->{message} }), @{$e->{children}} ];
 	my $box = create_widgets_block($o, $children, $update, $ignore_ref);
-	$w = gtknew('Expander', text => $e->{text}, child => $box);
+	$w = gtknew('Button', child => gtknew('HBox', spacing => 5, 
+                                              children_tight => [
+                                                  gtknew('Image', file => 'advanced_expander'),
+                                                  gtknew('Label', text => $e->{text}),
+                                              ],
+                                          ),
+                    if_($::isInstall, relief => 'none'),
+                    clicked => sub { ask_fromW($o, { }, $e->{children}) }
+                );
     } elsif ($e->{type} =~ /list/) {
 
 	$e->{formatted_list} = [ map { may_apply($e->{format}, $_) } @{$e->{list}} ];

commit 0c1924d63aaa1346e915c7f21a6f786ddcc5948c

(create_widget) restore support for advanced_messages (regression introduced in r244088: "(create_widget) popup advanced settings in dialogs")
git-svn-id: svn+ssh://svn.mandriva.com/svn/soft/drakx/trunk@244447 99302b65-d5f7-0310-b3dd-f8cd6f4e3d94

perl-install/interactive/gtk.pm
@@ -11,6 +11,7 @@ use common;
 use mygtk2;
 use ugtk2 qw(:helpers :wrappers :create);
 use Gtk2::Gdk::Keysyms;
+use feature qw(state);
 
 my $forgetTime = 1000; #- in milli-seconds
 
@@ -422,7 +423,15 @@ sub create_widget {
 	my $box = create_widgets_block($o, $children, $update, $ignore_ref);
 	$w = gtknew('HBox', children_tight => [
             gtknew('Install_Button', text => $e->{text},
-                   clicked => sub { ask_fromW($o, { title => N("Advanced") }, $e->{children}) }
+                   clicked => sub {
+                       state $done;
+                       if (!$done) {
+                           unshift @{$e->{children}},
+                             { type => 'only_label', no_indent => 1, val => \$e->{message} } if $e->{message}; 
+                           $done = 1;
+                       }
+        
+                       ask_fromW($o, { title => N("Advanced") }, $e->{children}) }
                )
         ]);
     } elsif ($e->{type} =~ /list/) {