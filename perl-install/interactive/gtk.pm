OpenMandrivaSoftware/drakx

commit f315a90de1e9d6a9dd151cd5a06392d187c837ea

(create_widget) workaround infamous 6 years old gnome bug #101968 (bad label wrapping)
git-svn-id: svn+ssh://svn.mandriva.com/svn/soft/drakx/trunk@244432 99302b65-d5f7-0310-b3dd-f8cd6f4e3d94

perl-install/interactive/gtk.pm
@@ -386,7 +386,8 @@ sub create_widget {
 	$set = sub { $w->set_active($_[0]) };
     } elsif ($e->{type} eq 'only_label') {
 	$w = $e->{title} ? 
-	         gtknew('Title2', label => escape_text_for_TextView_markup_format(${$e->{val}})) :
+	         gtknew('Title2', label => escape_text_for_TextView_markup_format(${$e->{val}}),
+                        width => mygtk2::get_label_width()) :
 		 gtknew('Label_Left', line_wrap => 1, text_markup => ${$e->{val}});
     } elsif ($e->{type} eq 'label') {
 	$w = gtknew('WrappedLabel', text_markup => ${$e->{val}});

commit 5ef8d2b7a2a5ab94aa547ac174fbdec11d7b7a70

(create_widgets_block) fix right aligned widgets
(regression introduced in r244432: "(create_widget) workaround
infamous 6 years old gnome bug #101968 (bad label wrapping)")


git-svn-id: svn+ssh://svn.mandriva.com/svn/soft/drakx/trunk@244438 99302b65-d5f7-0310-b3dd-f8cd6f4e3d94

perl-install/interactive/gtk.pm
@@ -387,7 +387,7 @@ sub create_widget {
     } elsif ($e->{type} eq 'only_label') {
 	$w = $e->{title} ? 
 	         gtknew('Title2', label => escape_text_for_TextView_markup_format(${$e->{val}}),
-                        width => mygtk2::get_label_width()) :
+                        if_($e->{alignment} ne 'right', width => mygtk2::get_label_width())) :
 		 gtknew('Label_Left', line_wrap => 1, text_markup => ${$e->{val}});
     } elsif ($e->{type} eq 'label') {
 	$w = gtknew('WrappedLabel', text_markup => ${$e->{val}});