OpenMandrivaSoftware/drakx

commit e0b228360b4bd5559699ad4765ccd71db29b7ab1

(level_choose) refresh "Security" step
git-svn-id: svn+ssh://svn.mandriva.com/svn/soft/drakx/trunk@244534 99302b65-d5f7-0310-b3dd-f8cd6f4e3d94

perl-install/security/level.pm
@@ -53,15 +53,17 @@ connections from many clients. Note: if your machine is only a client on the Int
     my @l = 2 .. 5;
 
     $in->ask_from_({ title => $::isInstall ? N("Security") : N("DrakSec Basic Options"),
-		     messages => N("Please choose the desired security level") . "\n\n" .
-		                 join('', map { to_string($_) . ": " . formatAlaTeX($help{$_}) . "\n\n" } @l),
 		     interactive_help_id => 'miscellaneous',
 		   }, [
-              { label => N("Security level"), val => $security, list => \@l, format => \&to_string },
+              { label => N("Please choose the desired security level"), title => 1 },
+              { val => $security, list => \@l, format => \&to_string,
+                format => sub { warn ">>$_\n"; N("%s: %s", to_string($_), formatAlaTeX($help{$_})) },
+                type => 'list', gtk => { use_boxradio => 1 } },
                 if_($in->do_pkgs->is_installed('libsafe') && arch() =~ /^i.86/,
                 { label => N("Use libsafe for servers"), val => $libsafe, type => 'bool', text =>
                   N("A library which defends against buffer overflow and format string attacks.") }),
-                { label => N("Security Administrator (login or email)"), val => $email, },
+                { label => N("Security Administrator:"), title => 1 },
+                { label => N("Login or email:"), val => $email, },
             ],
     );
 }

commit 159b33eb84235c4caf89595cb50fe951160f37dc

(level_choose) fix defaulting to current security level in GUI (regression introduced in r244534 on 2008-08-29, was: '(level_choose) refresh "Security" step'
git-svn-id: svn+ssh://svn.mandriva.com/svn/soft/drakx/trunk@245214 99302b65-d5f7-0310-b3dd-f8cd6f4e3d94

perl-install/security/level.pm
@@ -59,7 +59,7 @@ connections from many clients. Note: if your machine is only a client on the Int
               { val => $security, list => \@l, 
                 format => sub {
                     #-PO: this string is used to properly format "<security level>: <level description>"
-                    N("%s: %s", to_string($_), formatAlaTeX($help{$_}));
+                    N("%s[:] %s", to_string($_[0]), formatAlaTeX($help{$_[0]}));
                 },
                 type => 'list', gtk => { use_boxradio => 1 } },
                 { label => N("Security Administrator:"), title => 1 },