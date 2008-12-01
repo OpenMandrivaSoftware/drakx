OpenMandrivaSoftware/drakx

commit 689946ac50b82886901f82a81f268cb9318b44dc

- do not propose to upgrade non-mandriva and non-conectiva distros (#38408)
git-svn-id: svn+ssh://svn.mandriva.com/svn/soft/drakx/trunk@239304 99302b65-d5f7-0310-b3dd-f8cd6f4e3d94

perl-install/install/any.pm
@@ -940,6 +940,13 @@ sub generate_automatic_stage1_params {
 sub find_root_parts {
     my ($fstab, $prefix) = @_;
 
+    grep { $_->{release} =~ /\b(mandrake|mandriva|conectiva)\b/i } 
+      _find_root_parts($fstab, $prefix);
+}
+
+sub _find_root_parts {
+    my ($fstab, $prefix) = @_;
+
     if ($::local_install) {
 	my $f = common::release_file('/mnt') or return;
 	return common::parse_release_file('/mnt', $f, {});

commit 1703a47ffd878007a26661414f13d4ea1a476176

- do allow to upgrade Mandrakelinux (regression introduced in 10.14)
(bug tested on Mandrakelinux LE 2005)
(regression introduced in r239304 2008-03-13)


git-svn-id: svn+ssh://svn.mandriva.com/svn/soft/drakx/trunk@250060 99302b65-d5f7-0310-b3dd-f8cd6f4e3d94

perl-install/install/any.pm
@@ -936,7 +936,7 @@ sub generate_automatic_stage1_params {
 sub find_root_parts {
     my ($fstab, $prefix) = @_;
 
-    grep { $_->{release} =~ /\b(mandrake|mandriva|conectiva)\b/i } 
+    grep { $_->{release} =~ /\b(mandrake|mandrakelinux|mandriva|conectiva)\b/i } 
       _find_root_parts($fstab, $prefix);
 }
 