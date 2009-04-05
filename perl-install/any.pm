OpenMandrivaSoftware/drakx

commit 2bd36baef4c75a6a125993cba07d66b3cca85b6b

(ask_user_and_root) in order to guess the login name, just transliterate the real name into ascii rather than truncate it on first non ascii character
git-svn-id: svn+ssh://svn.mandriva.com/svn/soft/drakx/trunk@255158 99302b65-d5f7-0310-b3dd-f8cd6f4e3d94

perl-install/any.pm
@@ -816,7 +816,8 @@ sub ask_user_and_root {
             alignment => 'right', format => \&translate },
            ),
 	  { label => N("Real name"), val => \$u->{realname}, alignment => 'right', focus_out => sub {
-		$u->{name} ||= lc first($u->{realname} =~ /([a-zA-Z0-9_-]+)/);
+		$u->{name} ||= Locale::gettext::iconv($u->{realname}, "utf-8", "ascii//TRANSLIT");
+                $u->{name} =~ s/[^a-zA-Z0-9_-]//g; # drop any charcter that would break login program
 	    } },
 
           { label => N("Login name"), val => \$u->{name}, list => \@suggested_names, alignment => 'right',

commit 2abbb58a4d215862978b311a1b05f9a7d4f8dcdd

(ask_user_and_root) ensure login is lowercase when transliterating from real name (#49573)
(regression introduced in r255158 on 2009-03-30)


git-svn-id: svn+ssh://svn.mandriva.com/svn/soft/drakx/trunk@255569 99302b65-d5f7-0310-b3dd-f8cd6f4e3d94

perl-install/any.pm
@@ -816,7 +816,7 @@ sub ask_user_and_root {
             alignment => 'right', format => \&translate },
            ),
 	  { label => N("Real name"), val => \$u->{realname}, alignment => 'right', focus_out => sub {
-		$u->{name} ||= Locale::gettext::iconv($u->{realname}, "utf-8", "ascii//TRANSLIT");
+		$u->{name} ||= lc(Locale::gettext::iconv($u->{realname}, "utf-8", "ascii//TRANSLIT"));
                 $u->{name} =~ s/[^a-zA-Z0-9_-]//g; # drop any charcter that would break login program
 	    } },
 