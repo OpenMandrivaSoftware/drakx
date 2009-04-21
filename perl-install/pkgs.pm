OpenMandrivaSoftware/drakx

commit c40bb45cd12160fa00351de750099c803aeab8f7

(detect_hardware_packages,simple_read_rpmsrate) do not offer to install KDE packages on GNOME or LXDE (#49671)
git-svn-id: svn+ssh://svn.mandriva.com/svn/soft/drakx/trunk@256659 99302b65-d5f7-0310-b3dd-f8cd6f4e3d94

perl-install/pkgs.pm
@@ -149,14 +149,25 @@ sub read_rpmsrate {
 
 
 sub simple_read_rpmsrate {
-    my ($o_match_all_hardware) = @_;
+    my ($o_match_all_hardware, $o_ignore_flags) = @_;
     my ($rates, $flags) = read_rpmsrate({}, {}, $::prefix . '/usr/share/meta-task/rpmsrate-raw', $o_match_all_hardware);
+
+    # FIXME: we do not handle !CAT_desktop but we do not care for now:
+    if (!$o_match_all_hardware && $o_ignore_flags) {
+        while (my ($pkg, $pkg_flags) = each %$flags) {
+            my $flags_str = "@$pkg_flags";
+            if ($flags_str =~ /TRUE/ && any { $flags_str =~ /[^!]$_/ } @$o_ignore_flags) {
+                delete $flags->{$pkg};
+            }
+        }
+    }
+
     grep { member('TRUE', @{$flags->{$_}}) && $rates->{$_} >= 5 } keys %$flags;
 }
 
 sub detect_rpmsrate_hardware_packages {
-    my ($o_match_all_hardware) = @_;
-    grep { !/openoffice/ } simple_read_rpmsrate($o_match_all_hardware);
+    my ($o_match_all_hardware, $ignore_flags) = @_;
+    grep { !/openoffice/ } simple_read_rpmsrate($o_match_all_hardware, $ignore_flags);
 }
 
 sub detect_graphical_drivers {
@@ -204,8 +215,12 @@ sub detect_network_drivers {
 
 sub detect_hardware_packages {
     my ($do_pkgs, $o_match_all_hardware) = @_;
+    my @ignore_flags = $::isInstall ? () : (
+        if_(!$do_pkgs->are_installed('task-kde4-minimal', 'task-kde3-minimal'), "CAT_KDE"),
+        if_(!$do_pkgs->is_installed('task-gnome'), "CAT_GNOME"),
+    );
     (
-        ($::isInstall ? () : detect_rpmsrate_hardware_packages($o_match_all_hardware)),
+        ($::isInstall ? () : detect_rpmsrate_hardware_packages($o_match_all_hardware, \@ignore_flags)),
         detect_graphical_drivers($do_pkgs, $o_match_all_hardware),
         detect_network_drivers($do_pkgs, $o_match_all_hardware),
     );

commit 412ec5ac6faa8385f32b4b92e6bf6eac1ee4aaac

(read_rpmsrate) use the installer's logic regarding flags in standalone mode, thus fixing wrongly selecting some 32bits packages on x86_64 (#50148)
(regression introduced in r256659 on 2009-04-17;
"(detect_hardware_packages,simple_read_rpmsrate) do not offer to
install KDE packages on GNOME or LXDE (#49671)")


git-svn-id: svn+ssh://svn.mandriva.com/svn/soft/drakx/trunk@256910 99302b65-d5f7-0310-b3dd-f8cd6f4e3d94

perl-install/pkgs.pm
@@ -136,9 +136,10 @@ sub read_rpmsrate {
 	    $ok ? 'TRUE' : @$user_flags ? join('||', @$user_flags) : 'FALSE';
 	} @flags;
 
+	@flags = member('FALSE', @flags) ? 'FALSE' : @flags;
 	if ($::isInstall) {
             $p->set_rate($rates->{$_});
-            $p->set_rflags(member('FALSE', @flags) ? 'FALSE' : @flags);
+            $p->set_rflags(@flags);
 	} else {
             $flags->{$_} = \@flags;
 	}