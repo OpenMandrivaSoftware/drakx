OpenMandrivaSoftware/drakx

commit f0f5e10e0adf59202ca1c1c2a11cf79ed9f34002

Don't list installed distros with other archs as ugrading betweenarchs is not supported
git-svn-id: svn+ssh://svn.mandriva.com/svn/soft/drakx/trunk@255238 99302b65-d5f7-0310-b3dd-f8cd6f4e3d94

perl-install/install/steps_interactive.pm
@@ -126,6 +126,9 @@ sub selectInstallClass {
     my ($o) = @_;
 
     if (my @l = install::any::find_root_parts($o->{fstab}, $::prefix)) {
+	# Don't list other archs as ugrading between archs is not supported
+	@l = grep { $_->{arch} eq arch() } @l;
+
 	log::l("proposing to upgrade partitions " . join(" ", map { $_->{part} && $_->{part}{device} } @l));
 
 	my @releases = uniq(map { $_->{release} } @l);
@@ -147,15 +150,6 @@ sub selectInstallClass {
 			  format => sub { ref($_[0]) ? N("Upgrade %s", $_[0]{release}) : translate($_[0]) }
 			} ]);
 	if (ref $p) {
-	    if (arch() =~ /x86_64/ && $p->{arch} eq 'i586') {
-		$o->ask_warn('', N("Upgrade from a 32bit to a 64bit distribution is not supported"));
-		goto askInstallClass;
-	    }
-	    if (arch() =~ /i.86/ && $p->{arch} eq 'x86_64') {
-		$o->ask_warn('', N("Upgrade from a 64bit to a 32bit distribution is not supported"));
-		goto askInstallClass;
-	    }
-
 	    _check_unsafe_upgrade_and_warn($o, $p->{part}) or $p = undef;
 	}
 

commit 4f1f76213d8d59109b8100d18060a1f60f291b15

(selectInstallClass) fix not offering to upgrade 32 bit installations
(regression introduced by pterjan in r255238 on 2009-03-31: "Don't list
installed distros with other archs as ugrading betweenarchs is not supported")


git-svn-id: svn+ssh://svn.mandriva.com/svn/soft/drakx/trunk@257045 99302b65-d5f7-0310-b3dd-f8cd6f4e3d94

perl-install/install/steps_interactive.pm
@@ -127,7 +127,8 @@ sub selectInstallClass {
 
     my @l = install::any::find_root_parts($o->{fstab}, $::prefix);
     # Don't list other archs as ugrading between archs is not supported
-    @l = grep { $_->{arch} eq arch() } @l;
+    my $arch = arch() =~ /i.86/ ? $MDK::Common::System::compat_arch{arch()} : arch();
+    @l = grep { $_->{arch} eq $arch } @l;
     if (@l) {
 
 	log::l("proposing to upgrade partitions " . join(" ", map { $_->{part} && $_->{part}{device} } @l));