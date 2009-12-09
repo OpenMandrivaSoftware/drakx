OpenMandrivaSoftware/drakx

commit b8c1cbca8277ca2ef39cecf04d1aa9394e31ad72

do not stay chdired in /mnt/var/cache/urpmi when download fails (workaround urpmi bug)
git-svn-id: svn+ssh://svn.mandriva.com/svn/soft/drakx/trunk@263663 99302b65-d5f7-0310-b3dd-f8cd6f4e3d94

perl-install/install/http.pm
@@ -54,7 +54,13 @@ sub get_file_and_size {
         urpm::download::set_cmdline_proxy(http_proxy => "http://$proxy/");
     }
     
+    (my $cwd) = getcwd() =~ /(.*)/;
     my $res = eval { urpm::download::sync_url($urpm, $url, %o_options, dir => $cachedir) };
+    #- urpmi does not always reset cwd when dying or failing!
+    #- do not stay in /mnt/var/cache/urpmi, it's evil to be in /mnt
+    #- FIXME: fix urpmi
+    chdir $cwd;
+
     if ($res) {
         open(my $f, $file);
         (-s $file, $f);

commit 4fdbadbcebe4f416ce735b5748ec5aff46eb4dff

fix crash introduced by r263663 (blino, 2009-11-24) (was: "do not stay chdired in /mnt/var/cache/urpmi when download fails (workaround urpmi bug) 'do not stay chdired in /mnt/var/cache/urpmi'")
git-svn-id: svn+ssh://svn.mandriva.com/svn/soft/drakx/trunk@263956 99302b65-d5f7-0310-b3dd-f8cd6f4e3d94

perl-install/install/http.pm
@@ -2,6 +2,7 @@ package install::http; # $Id$
 
 use urpm::download;
 use common;
+use Cwd;
 
 # to be killed once callers got fixed
 sub close() {