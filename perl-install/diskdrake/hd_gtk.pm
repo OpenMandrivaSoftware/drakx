OpenMandrivaSoftware/drakx

commit d15c8640e6d0d87857ae24992d7977f82de8328d

split button bars into 3 bars
git-svn-id: svn+ssh://svn.mandriva.com/svn/soft/drakx/trunk@245228 99302b65-d5f7-0310-b3dd-f8cd6f4e3d94

perl-install/diskdrake/hd_gtk.pm
@@ -70,7 +70,8 @@ sub main {
                               width => mygtk2::get_label_width()
                             ),
 		    1, (my $notebook_widget = Gtk2::Notebook->new),
-		    0, (my $per_kind_action_box = Gtk2::HBox->new(0,0)),
+		    0, (my $per_kind_action_box = gtknew('HButtonBox', layout => 'edge')),
+		    0, (my $per_kind_action_box2 = gtknew('HButtonBox', layout => 'end')),
 		    0, (my $general_action_box  = gtknew('HButtonBox', spacing => 5, layout => 'spread')),
 		   ),
 	  );
@@ -84,7 +85,7 @@ sub main {
 	partition_table::assign_device_numbers($_) foreach fs::get::hds($all_hds);
 	create_automatic_notebooks($notebook_widget);
 	general_action_box($general_action_box);
-	per_kind_action_box($per_kind_action_box, $current_kind);
+	per_kind_action_box($per_kind_action_box, $per_kind_action_box2, $current_kind);
 	current_kind_changed($in, $current_kind);
 	current_entry_changed($current_kind, $current_entry);
 	$lock = 0;
@@ -184,13 +185,18 @@ sub general_action_box {
     }
 }
 sub per_kind_action_box {
-    my ($box, $kind) = @_;
-    $_->destroy foreach $box->get_children;
+    my ($box, $box2, $kind) = @_;
+    $_->destroy foreach $box->get_children, $box2->get_children;
 
     $kind->{type} =~ /hd|lvm/ or return;
 
-    foreach my $s (diskdrake::interactive::hd_possible_actions($in, kind2hd($kind), $all_hds)) {
+    foreach my $s (diskdrake::interactive::hd_possible_actions_base($in, kind2hd($kind), $all_hds)) {
 	gtkadd($box, 
+	       gtksignal_connect(Gtk2::Button->new(translate($s)),
+				 clicked => sub { try($s, kind2hd($kind)) }));
+    }
+    foreach my $s (diskdrake::interactive::hd_possible_actions_extra($in, kind2hd($kind), $all_hds)) {
+	gtkadd($box2, 
 	       gtksignal_connect(Gtk2::Button->new(translate($s)),
 				 clicked => sub { try($s, kind2hd($kind)) }));
     }

perl-install/diskdrake/interactive.pm
@@ -256,7 +256,7 @@ sub main {
 ################################################################################
 sub general_possible_actions {
     my ($_in, $_all_hds) = @_;
-    $::expert ? N_("Toggle to normal mode") : N_("Toggle to expert mode");
+    if_($::isInstall, N_("More"));
 }
 
 sub Done {
@@ -296,14 +296,26 @@ Quit anyway?", $part->{device}, $part->{mntpoint})) or return if $::isStandalone
 ################################################################################
 # per-hd actions
 ################################################################################
-sub hd_possible_actions {
-    my ($_in, $hd, $_all_hds) = @_;
+sub hd_possible_actions_base {
+    my ($hd) = @_;
     ( 
      if_(!$hd->{readonly} || $hd->{getting_rid_of_readonly_allowed}, N_("Clear all")), 
      if_(!$hd->{readonly} && $::isInstall, N_("Auto allocate")),
-     if_($::isInstall, N_("More")),
     );
 }
+
+sub hd_possible_actions_extra {
+    my ($hd) = @_;
+    $::expert ? N_("Toggle to normal mode") : N_("Toggle to expert mode");
+}
+
+
+sub hd_possible_actions {
+    my ($_in, $hd, $_all_hds) = @_;
+    hd_possible_actions_base($hd);
+    hd_possible_actions_extra($hd);
+}
+
 sub hd_possible_actions_interactive {
     my ($_in, $_hd, $_all_hds) = @_;
     &hd_possible_actions, N_("Hard drive information");

commit a39f2d58ea6bda2d37940ed2d013f00cda4d106d

Author: tv <tv@5b7c3c08-40e5-403b-9995-ace06908e4af> Date:   Fri Mar 23 19:50:50 2012 +0000
    (per_kind_action_box) fix too many parameters
    
    issue introduced in commit r245228 on Sep 15 2008
    (was: "split button bars into 3 bars")
    
    (spot by perl_checker)



git-svn-id: svn+ssh://svn.mandriva.com/svn/soft/drakx/trunk@274084 99302b65-d5f7-0310-b3dd-f8cd6f4e3d94

perl-install/diskdrake/hd_gtk.pm
@@ -199,12 +199,12 @@ sub per_kind_action_box {
 
     $kind->{type} =~ /hd|lvm/ or return;
 
-    foreach my $s (diskdrake::interactive::hd_possible_actions_base($in, kind2hd($kind), $all_hds)) {
+    foreach my $s (diskdrake::interactive::hd_possible_actions_base($in)) {
 	gtkadd($box, 
 	       gtksignal_connect(Gtk2::Button->new(translate($s)),
 				 clicked => sub { try($s, kind2hd($kind)) }));
     }
-    foreach my $s (diskdrake::interactive::hd_possible_actions_extra($in, kind2hd($kind), $all_hds)) {
+    foreach my $s (diskdrake::interactive::hd_possible_actions_extra($in)) {
 	gtkadd($box2, 
 	       gtksignal_connect(Gtk2::Button->new(translate($s)),
 				 clicked => sub { try($s, kind2hd($kind)) }));