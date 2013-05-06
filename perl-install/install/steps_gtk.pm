OpenMandrivaSoftware/drakx

commit 39c4542c52981ac9f8e751be1463576f340d87eb

(installCallback) fix doble logging of %(pre|post)trans packages
eg: 'filesystem' which has a %pretrans scriptlet (mga#7758)

git-svn-id: svn://svn.mageia.org/soft/drakx/trunk@8029 5b7c3c08-40e5-403b-9995-ace06908e4af

perl-install/install/steps_gtk.pm
@@ -650,7 +650,7 @@ sub installPackages {
 	    $o->{install_start_time} = 0;
 	    mygtk2::gtkadd($pkg_log_widget, text => P("%d package", "%d packages", $nb, $nb));
 	    $w->flush;
-	} elsif ($type eq 'open') {
+	} elsif ($type eq 'inst' && $subtype eq 'start') {
 	    gtkval_modify(\$pkg_progress, 0);
 	    my $p = $packages->{depslist}[$id];
 	    mygtk2::gtkadd($pkg_log_widget, text => sprintf("\n%s: %s", $p->name, translate($p->summary)));

commit 1f209d59028d36779e52333c56544fd53c64425e

fix displaying ads (mga#9939)
regression introduced in commit r8029 on 2013-04-26:
"fix doble logging of 'filesystem' installation (mga#7758)"

git-svn-id: svn://svn.mageia.org/soft/drakx/trunk@8121 5b7c3c08-40e5-403b-9995-ace06908e4af

perl-install/install/steps_gtk.pm
@@ -650,13 +650,14 @@ sub installPackages {
 	    $o->{install_start_time} = 0;
 	    mygtk2::gtkadd($pkg_log_widget, text => P("%d package", "%d packages", $nb, $nb));
 	    $w->flush;
+	} elsif ($type eq 'open') {
+	    $advertize->(1) if $show_advertising && $total_size > 20_000_000 && time() - $change_time > 20;
 	} elsif ($type eq 'inst' && $subtype eq 'start') {
 	    gtkval_modify(\$pkg_progress, 0);
 	    my $p = $packages->{depslist}[$id];
 	    mygtk2::gtkadd($pkg_log_widget, text => sprintf("\n%s: %s", $p->name, translate($p->summary)));
 	    $current_total_size += $last_size;
 	    $last_size = $p->size;
-	    $advertize->(1) if $show_advertising && $total_size > 20_000_000 && time() - $change_time > 20;
 
             # display release notes if requested, when not chrooted:
             if ($show_release_notes) {