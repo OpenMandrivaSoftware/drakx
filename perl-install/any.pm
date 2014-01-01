0f15008bd8d3d71beffc9a5c6f3cae35bb752ee2
tpg (Tomasz Paweł Gajc) <tpgxyz@gmail.com>
2014-01-01 23:02:28 +0100

    correct locale

--- perl-install/any.pm
+++ perl-install/any.pm
@@ -1098,6 +1098,7 @@ sub get_release_notes {
     my ($in) = @_;
     my $ext = $in->isa('interactive::gtk') ? '.html' : '.txt';
     my $separator = $in->isa('interactive::gtk') ? "\n\n" : '';
+    my $lang = (($ENV{'LC_MESSAGES'} =~ m/ru_RU/) ? 'ru' : 'en');
 
     my $release_notes = join($separator, grep { $_ } map {
         if ($::isInstall) {
