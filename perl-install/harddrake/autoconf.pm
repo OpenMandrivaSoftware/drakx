769f56d6c1658cf16d5b3cd65f1d3efd57e4f66b
tpg (Tomasz Paweł Gajc) <tpgxyz@gmail.com>
2014-01-01 19:26:16 +0100

    fix detect devices

--- perl-install/harddrake/autoconf.pm
+++ perl-install/harddrake/autoconf.pm
@@ -24,10 +24,43 @@ sub xconf {
     modules::load_category($modules_conf, 'various/agpgart'); 
 }
 
+sub setup_ethernet_device {
+    my ($in, $device) = @_;
+
+    require network::connection;
+    require network::connection::ethernet;
+    require network::connection::wireless;
+    my @connection_types = qw(network::connection::ethernet  network::connection::wireless);
+    my @all_connections = map { $_->get_connections(automatic_only => 1) } @connection_types;
+    my $interface = network::connection::ethernet::device_to_interface($device)
+      or return;
+    my $connection = find { $_->get_interface eq $interface } @all_connections
+      or return;
+
+    require network::connection_manager;
+    my $net = {};
+    network::network::read_net_conf($net);
+    my $cmanager = network::connection_manager->new($in, $net);
+    $cmanager->set_connection($connection);
+
+    # this will installed required packages
+    $cmanager->setup_connection;
+}
+
 sub network_conf {
-    my ($obj) = @_;
-    require network::network;
-    network::network::easy_dhcp($obj->{net}, $obj->{modules_conf});
+    my ($modules_conf, $in, $added) = @_;
+    $modules_conf->remove_alias_regexp('^(wlan|eth)[0-9]*$');
+    modules::load_category($modules_conf, 'network/main|gigabit|usb|wireless|firewire|pcmcia');
+
+    setup_ethernet_device($in, $_) foreach @{$added || {}};
+
+    require network::connection::ethernet;
+    network::connection::ethernet::configure_eth_aliases($modules_conf);
+    require network::rfswitch;
+    network::rfswitch::configure();
+    require network::shorewall;
+    network::shorewall::update_interfaces_list();
+    $modules_conf->write;
 }
 
 sub mouse_conf {
@@ -46,6 +79,11 @@ sub pcmcia {
 
 sub bluetooth {
     my ($enable) = @_;
+    # do not disable bluetooth service if adapter disappears
+    # (for example if disabled by Fn keys)
+    # systemd will automatically disable the service if needed
+    return if !$enable;
+
 #- FIXME: make sure these packages are installed when needed
 #     if ($enable) {
 #         require do_pkgs;
@@ -66,7 +104,7 @@ sub laptop {
 #     require do_pkgs;
 #     my $do_pkgs = do_pkgs_standalone->new;
 #     if ($on_laptop) {
-#         $do_pkgs->ensure_is_installed("cpupower", "/lib/systemd/system/cpupower.service");
+#         $do_pkgs->ensure_is_installed("cpufreq", "/etc/rc.d/init.d/cpufreq");
 #         $do_pkgs->ensure_is_installed("apmd", "/usr/bin/apm");
 #         $do_pkgs->ensure_is_installed("hotkeys", "/usr/bin/hotkeys");
 #         $do_pkgs->ensure_is_installed("laptop-mode-tools", "/usr/sbin/laptop_mode");
@@ -75,15 +113,13 @@ sub laptop {
 #     }
     require services;
     services::set_status("apmd", -e "/proc/apm");
-    #services::set_status("laptop-mode", $on_laptop);
+    services::set_status("laptop-mode", $on_laptop);
     services::set_status("numlock", !$on_laptop);
-    #services::set_status("cpupower", $on_laptop);
-    
 }
 
-sub cpupower() {
-    require cpupower;
-    modules::set_preload_modules("cpupower", cpupower::get_modules());
+sub cpufreq() {
+    require cpufreq;
+    modules::set_preload_modules("cpufreq", cpufreq::get_modules());
 }
 
 sub floppy() {
