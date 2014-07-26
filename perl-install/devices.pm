e61c684e3eb67b23fc2aa749fa4d03db68d8bba6
tpg (Tomasz Paweł Gajc) <tpgxyz@gmail.com>
2014-07-26 20:46:54 +0200

    get rid of some ancient stuff, udev takes care of everything nowadays

--- perl-install/devices.pm
+++ perl-install/devices.pm
@@ -49,22 +49,9 @@ sub find_compressed_image {
     undef;
 }
 
-sub get_dynamic_major {
-    my ($name) = @_;
-    cat_('/proc/devices') =~ /^(\d+) \Q$name\E$/m && $1;
-}
-
 sub init_device_mapper() {
     require modules;
     eval { modules::load('dm-mod') };
-    make('urandom');
-    my $control = '/dev/mapper/control';
-    if (! -e $control) {
-	my ($major) = get_dynamic_major('misc') or return;
-	my ($minor) = cat_('/proc/misc') =~ /(\d+) device-mapper$/m or return;
-	mkdir_p(dirname($control));
-	syscall_('mknod', $control, c::S_IFCHR() | 0600, makedev($major, $minor)) or die "mknod $control failed: $!";	
-    }
 }
 
 sub entry {
@@ -88,25 +75,10 @@ sub entry {
 	$type = c::S_IFBLK();
 	$major = ($1 eq 'ida' ? 72 : 104) + $2;
 	$minor = 16 * $3 + ($4 || 0);
-    } elsif (m,(ataraid)/d(\d+)(?:p(\d+))?,) {
-	# ATA raid "ataraid/d0{p1}"
-	$type = c::S_IFBLK();
-	$major = 114;
-	$minor = 16 * $1 + ($2 || 0);
     } elsif (my ($prefix, $nb) = /(.*?)(\d+)$/) {	
 	my $f = ${{
-		   "hidbp-mse-"  => sub { c::S_IFCHR(), 10, 32 },
-		   "lp"          => sub { c::S_IFCHR(), 6,  0  },
-		   "usb/lp"      => sub { c::S_IFCHR(), 180, 0 },
-		   "input/event" => sub { c::S_IFCHR(), 13, 64 },
-		   "loop"        => sub { c::S_IFBLK(), 7,  0  },
-		   "md"          => sub { c::S_IFBLK(), 9,  0  },
-		   "nst"         => sub { c::S_IFCHR(), 9, 128 },
 		   "sr"          => sub { c::S_IFBLK(), 11, 0  },
-		   "tty"         => sub { c::S_IFCHR(), 4,  0  },
-		   "ttyS"        => sub { c::S_IFCHR(), 4, 64  },
 		   "ubd/"        => sub { c::S_IFBLK(), 98, 0  },
-		   "dm-"         => sub { c::S_IFBLK(), get_dynamic_major('device-mapper'), 0 },
 	       }}{$prefix};
 	if ($f) {
 	    ($type, $major, $minor) = $f->();
@@ -115,35 +87,9 @@ sub entry {
     }
     unless ($type) {
 	($type, $major, $minor) =
-	     @{ ${{"aztcd"    => [ c::S_IFBLK(), 29, 0  ],
-		   "bpcd"     => [ c::S_IFBLK(), 41, 0  ],
-		   "cdu31a"   => [ c::S_IFBLK(), 15, 0  ],
-		   "cdu535"   => [ c::S_IFBLK(), 24, 0  ],
-		   "cm206cd"  => [ c::S_IFBLK(), 32, 0  ],
-		   "gscd"     => [ c::S_IFBLK(), 16, 0  ],
-		   "mcd"      => [ c::S_IFBLK(), 23, 0  ],
-		   "mcdx"     => [ c::S_IFBLK(), 20, 0  ],
-		   "mem"      => [ c::S_IFCHR(), 1,  1  ],
-		   "optcd"    => [ c::S_IFBLK(), 17, 0  ],
-		   "kbd"      => [ c::S_IFCHR(), 11, 0  ],
-		   "psaux"    => [ c::S_IFCHR(), 10, 1  ],
+	     @{ ${{
 		   "atibm"    => [ c::S_IFCHR(), 10, 3  ],
-		   "random"   => [ c::S_IFCHR(), 1,  8  ],
-		   "urandom"  => [ c::S_IFCHR(), 1,  9  ],
-		   "sbpcd"    => [ c::S_IFBLK(), 25, 0  ],
-		   "sjcd"     => [ c::S_IFBLK(), 18, 0  ],
-		   "tty"      => [ c::S_IFCHR(),  5, 0  ],
-		   "input/mice"
-		              => [ c::S_IFCHR(), 13, 63 ],
 		   "adbmouse" => [ c::S_IFCHR(), 10, 10 ], #- PPC
-		   "vcsa"     => [ c::S_IFCHR(), 7,  128 ],
-		   "zero"     => [ c::S_IFCHR(), 1,  5  ],		     
-		   "null"     => [ c::S_IFCHR(), 1,  3  ],		     
-
-		   "initrd"   => [ c::S_IFBLK(), 1,  250 ],
-		   "console"  => [ c::S_IFCHR(), 5,  1  ],
-		   "systty"   => [ c::S_IFCHR(), 4,  0  ],
-		   "lvm"   =>    [ c::S_IFBLK(), 109, 0 ],
 	       }}{$_} || [] };
     }
     # Lookup non listed devices in /sys
--- perl-install/fs/any.pm
+++ perl-install/fs/any.pm
@@ -81,8 +81,6 @@ sub create_minimal_files() {
 	home mnt run tmp var var/tmp var/lib var/lib/rpm var/lib/urpmi);
     mkdir "$::prefix/$_", 0700 foreach qw(root root/tmp root/drakx);
 
-    devices::make("$::prefix/dev/null");
-    chmod 0666, "$::prefix/dev/null";
 }
 
 sub prepare_minimal_root() {
