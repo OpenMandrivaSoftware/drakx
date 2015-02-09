35736de7ed3b99a5e52abbf19031145d51a63168
Tomasz Paweł Gajc <tpgxyz@gmail.com>
2015-02-09 18:24:42 +0400

    Ensure that /etc/localtime is a symlink from mageia

--- perl-install/timezone.pm
+++ perl-install/timezone.pm
@@ -70,6 +70,8 @@ sub write {
     set_ntp_server($t->{ntp});
 
     my $tz_prefix = get_timezone_prefix();
+    eval { symlinkf($tz_prefix . '/' . $t->{timezone}, "$::prefix/etc/localtime") };
+    $@ and log::l("installing /etc/localtime failed");
     run_program::run('timedatectl', 'set-timezone', $t->{timezone});
     $@ and log::l("installing /etc/localtime failed");
 
