fdcf347280a20a23d14f05a36ca9c1eb2e648e11
Crispin Boylan <crisb@mandriva.org>
2015-06-09 23:30:42 +0100

    Add ESP if rebuilding

--- perl-install/fs/partitioning_wizard.pm
+++ perl-install/fs/partitioning_wizard.pm
@@ -245,6 +245,8 @@ filesystem checks will be run on your next boot into Microsoft Windows®")) if $
 				    title => N("Partitioning"),
 				    interactive_help_id => 'takeOverHdConfirm' }) or return;
 		fsedit::partition_table_clear_and_initialize($all_hds->{lvms}, $hd, $in);
+                my $fstab = [ fs::get::fstab($all_hds) ];
+                fsedit::init_efi_suggestions($fstab, 1)
 		fsedit::auto_allocate($all_hds, $partitions);
 		1;
 	    } ];
